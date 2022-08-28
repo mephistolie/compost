f91afec33fdcef78dd7e3e21371fde47f904c330
mephistolie <mikhaillevin@inbox.ru>
2022-08-28 13:46:52 +0300

    refactor(Clickable): update debounceClickable arguments

--- core/src/main/java/com/mephistolie/compost/modifiers/Clickable.kt
+++ core/src/main/java/com/mephistolie/compost/modifiers/Clickable.kt
@@ -2,14 +2,18 @@ package com.mephistolie.compost.modifiers
 
 import android.view.MotionEvent
 import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.foundation.Indication
+import androidx.compose.foundation.LocalIndication
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.PressInteraction
 import androidx.compose.runtime.*
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
 import androidx.compose.ui.draw.scale
 import androidx.compose.ui.input.pointer.pointerInteropFilter
+import androidx.compose.ui.semantics.Role
 
 /**
  * Configure component to receive clicks without ripple effect and extra args.
@@ -76,18 +80,73 @@ fun Modifier.scalingClickable(
 /**
  * Configure component to set the interval between successful clicks.
  *
- * @param debounceInterval minimum interval between two successful clicks
+ * @param enabled Controls the enabled state. When `false`, [onClick], and this modifier will
+ * appear disabled for accessibility services
+ * @param onClickLabel semantic / accessibility label for the [onClick] action
+ * @param role the type of user interface element. Accessibility services might use this
+ * to describe the element or do customizations
+ * @param debounceInterval minimum interval between two successful clicks. If null, standard clickable
+ * will be used
  * @param onClick will be called when the element is successfully clicked
  **/
 fun Modifier.debounceClickable(
-    debounceInterval: Long = 500L,
+    enabled: Boolean = true,
+    onClickLabel: String? = null,
+    role: Role? = null,
+    debounceInterval: Long? = 500L,
+    onClick: () -> Unit,
+): Modifier = composed {
+    debounceClickable(
+        interactionSource = remember { MutableInteractionSource() },
+        indication = LocalIndication.current,
+        enabled = enabled,
+        onClickLabel = onClickLabel,
+        role = role,
+        debounceInterval = debounceInterval,
+        onClick = onClick,
+    )
+}
+
+/**
+ * Configure component to set the interval between successful clicks.
+
+ * @param interactionSource [MutableInteractionSource] that will be used to dispatch
+ * [PressInteraction.Press] when this clickable is pressed. Only the initial (first) press will be
+ * recorded and dispatched with [MutableInteractionSource].
+ * @param indication indication to be shown when modified element is pressed. Be default,
+ * indication from [LocalIndication] will be used. Pass `null` to show no indication, or
+ * current value from [LocalIndication] to show theme default
+ * @param enabled Controls the enabled state. When `false`, [onClick], and this modifier will
+ * appear disabled for accessibility services
+ * @param onClickLabel semantic / accessibility label for the [onClick] action
+ * @param role the type of user interface element. Accessibility services might use this
+ * to describe the element or do customizations
+ * @param debounceInterval minimum interval between two successful clicks. If null, standard clickable
+ * will be used
+ * @param onClick will be called when the element is successfully clicked
+ **/
+fun Modifier.debounceClickable(
+    interactionSource: MutableInteractionSource,
+    indication: Indication?,
+    enabled: Boolean = true,
+    onClickLabel: String? = null,
+    role: Role? = null,
+    debounceInterval: Long? = 500L,
     onClick: () -> Unit,
 ): Modifier = composed {
     var lastClickTime by remember { mutableStateOf(0L) }
-    clickable {
-        val currentTime = System.currentTimeMillis()
-        if ((currentTime - lastClickTime) < debounceInterval) return@clickable
-        lastClickTime = currentTime
+    clickable(
+        interactionSource = interactionSource,
+        indication = indication,
+        enabled = enabled,
+        onClickLabel = onClickLabel,
+        role = role
+    ) {
+        if (debounceInterval != null) {
+            val currentTime = System.currentTimeMillis()
+            if ((currentTime - lastClickTime) < debounceInterval) return@clickable
+            lastClickTime = currentTime
+        }
         onClick()
     }
 }
